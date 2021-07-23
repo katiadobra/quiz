f8a150cc935a813812a53ecdeec4404408981b99
katiadobra <broun.kat@gmail.com>
2021-07-24 00:17:10 +0200

    add data and question component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="ctr">
-    <div class="questions-ctr">
-      <div class="progress">
-        <div class="bar"></div>
-        <div class="status">1 out of 3 questions answered</div>
-      </div>
-      <div class="single-question">
-        <div class="question">Sample Question 1</div>
-        <div class="answers">
-          <div class="answer">Sample Answer 1</div>
-          <div class="answer">Sample Answer 2</div>
-          <div class="answer">Sample Answer 3</div>
-          <div class="answer">Sample Answer 4</div>
-        </div>
-      </div>
-    </div>
-    <div class="result">
-      <div class="title">You got sample result 1!</div>
-      <div class="desc">Enter a short description here about the result.</div>
-    </div>
+    <question
+      v-if="questionsAnswered < questions.length"
+      :questions="questions"
+    />
+    <result v-else />
     <button type="button" class="reset-btn">Reset</button>
   </div>
 </template>
 
 <script>
+import Question from "./components/Question.vue";
+import Result from "./components/Result.vue";
+import data from "./assets/data.json";
 export default {
   name: "App",
+  components: {
+    Question,
+    Result,
+  },
+  data() {
+    return {
+      questionsAnswered: 0,
+      questions: data.questions,
+      results: data.results,
+    };
+  },
 };
 </script>
 
